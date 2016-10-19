ee3964a0f0a3cfecfd5b063a5734ebcaf9f5f9ae
katopz <katopz@gmail.com>
2016-10-19 21:20:34 +0700

    Working persist state with localStorage

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
+const store = createStore(reducer, persistedState)
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 
 render(
   <Provider store={store}>
